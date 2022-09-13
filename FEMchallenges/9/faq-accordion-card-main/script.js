325e1ba3ce8ec5d25397e60b3d0385cd20660b0a
uzairK134 <111652732+uzairK134@users.noreply.github.com>
2022-09-14 02:09:50 +0500

    Add files via upload

---
+++ FEMchallenges/9/faq-accordion-card-main/script.js
@@ -0,0 +1,13 @@
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) => question.addEventListener('click', () => {
+    if(question.parentNode.classList.contains('active')){
+        question.parentNode.classList.toggle('active');
+    }  
+    else{
+        questions.forEach((question) => question.parentNode.classList.remove('active'));
+        question.parentNode.classList.add('active');
+    }
+}));
+
